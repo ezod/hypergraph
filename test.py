2072934babd54a1ad6df84741638e6ed50f11843
Aaron Mavrinac <mavrinac@gmail.com>
2011-03-24 12:00:21 -0400

    Add core unit tests.

--- test.py
+++ test.py
@@ -11,6 +11,92 @@ Unit tests for hypergraph.
 
 import unittest
 
+from hypergraph import *
+from hypergraph.orientation import *
+
+
+class TestCore(unittest.TestCase):
+
+    def setUp(self):
+        V = set(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'])
+        self.U = Hypergraph(vertices=V)
+        self.U.add_edge(Edge(['A', 'D', 'C', 'I', 'G', 'H', 'B']), weight=9.805444)
+        self.U.add_edge(Edge(['E', 'D', 'F', 'A']), weight=7.944848)
+        self.U.add_edge(Edge(['F', 'B', 'C']), weight=5.238859)
+        self.U.add_edge(Edge(['D', 'E', 'J']), weight=2.182849)
+        self.U.add_edge(Edge(['B', 'C', 'E', 'A', 'I', 'G', 'F']), weight=1.700069)
+        self.U.add_edge(Edge(['I', 'H', 'G', 'J', 'C', 'D']), weight=7.809860)
+        self.U.add_edge(Edge(['G', 'F', 'E']), weight=8.340940)
+        self.U.add_edge(Edge(['D', 'G', 'E', 'H', 'F', 'C', 'B', 'A', 'I']), weight=6.847455)
+        self.U.add_edge(Edge(['J', 'C', 'H', 'B', 'F', 'D', 'E', 'A']), weight=9.601762)
+        self.U.add_edge(Edge(['J', 'D', 'G']), weight=2.771911)
+        self.U.add_edge(Edge(['F', 'G', 'I', 'H']), weight=9.884923)
+        self.U.add_edge(Edge(['D']), weight=1.910802)
+        self.U.add_edge(Edge(['C', 'J', 'H', 'E', 'G', 'F', 'A', 'I', 'D', 'B']), weight=2.443810)
+        self.U.add_edge(Edge(['A', 'G']), weight=9.445038)
+        self.U.add_edge(Edge(['G', 'H', 'D', 'I', 'A', 'J', 'E', 'B', 'F']), weight=0.320235)
+        self.U.add_edge(Edge(['I', 'D']), weight=4.417088)
+        self.U.add_edge(Edge(['E', 'G', 'I']), weight=5.241375)
+        self.U.add_edge(Edge(['B', 'J', 'A', 'H']), weight=5.715912)
+        self.U.add_edge(Edge(['I', 'D', 'E', 'B']), weight=4.940382)
+        self.U.add_edge(Edge(['H', 'C', 'I']), weight=2.983528)
+        self.D = Hypergraph(vertices=V, directed=True)
+        self.D.add_edge(Edge(['A', 'G'], 'A'), weight=9.445038)
+        self.D.add_edge(Edge(['A', 'C', 'B', 'E', 'D', 'G', 'F', 'I', 'H', 'J'], 'J'), weight=2.443810)
+        self.D.add_edge(Edge(['I', 'H', 'G', 'F'], 'F'), weight=9.884923)
+        self.D.add_edge(Edge(['J', 'E', 'D'], 'D'), weight=2.182849)
+        self.D.add_edge(Edge(['I', 'B', 'E', 'D'], 'B'), weight=4.940382)
+        self.D.add_edge(Edge(['C', 'D', 'G', 'I', 'H', 'J'], 'G'), weight=7.809860)
+        self.D.add_edge(Edge(['D'], 'D'), weight=1.910802)
+        self.D.add_edge(Edge(['A', 'C', 'B', 'E', 'G', 'F', 'I'], 'E'), weight=1.700069)
+        self.D.add_edge(Edge(['J', 'D', 'G'], 'G'), weight=2.771911)
+        self.D.add_edge(Edge(['E', 'G', 'F'], 'F'), weight=8.340940)
+        self.D.add_edge(Edge(['I', 'H', 'C'], 'C'), weight=2.983528)
+        self.D.add_edge(Edge(['C', 'B', 'F'], 'C'), weight=5.238859)
+        self.D.add_edge(Edge(['A', 'C', 'B', 'D', 'G', 'I', 'H'], 'I'), weight=9.805444)
+        self.D.add_edge(Edge(['A', 'E', 'D', 'F'], 'A'), weight=7.944848)
+        self.D.add_edge(Edge(['A', 'H', 'B', 'J'], 'H'), weight=5.715912)
+        self.D.add_edge(Edge(['A', 'B', 'E', 'D', 'G', 'F', 'I', 'H', 'J'], 'B'), weight=0.320235)
+        self.D.add_edge(Edge(['I', 'E', 'G'], 'I'), weight=5.241375)
+        self.D.add_edge(Edge(['I', 'D'], 'I'), weight=4.417088)
+        self.D.add_edge(Edge(['A', 'C', 'B', 'E', 'D', 'G', 'F', 'I', 'H'], 'E'), weight=6.847455)
+        self.D.add_edge(Edge(['A', 'C', 'B', 'E', 'D', 'F', 'H', 'J'], 'D'), weight=9.601762)
+
+    def test_equal_repr(self):
+        G = Hypergraph(vertices=self.U.vertices, edges=self.U.edges, weights=self.U.weights, directed=False)
+        self.assertTrue(G == self.U)
+        H = eval('%s' % self.U)
+        self.assertTrue(H == self.U)
+
+    def test_remove_vertex(self):
+        self.U.remove_vertex('I')
+        self.assertFalse('I' in self.U.vertices)
+        self.assertFalse(Edge(['I', 'D']) in self.U.edges)
+        self.assertFalse(Edge(['I', 'D']) in self.U.weights.keys())
+
+    def test_add_edge(self):
+        self.assertRaises(ValueError, self.U.add_edge, Edge(['A', 'Z']))
+
+    def test_remove_edge(self):
+        self.U.remove_edge(Edge(['I', 'D']))
+        self.assertFalse(Edge(['I', 'D']) in self.U.edges)
+        self.assertFalse(Edge(['I', 'D']) in self.U.weights.keys())
+
+    def test_adjacent(self):
+        self.assertTrue(self.U.adjacent('A', 'G'))
+        self.assertFalse(self.D.adjacent('A', 'G'))
+        self.assertTrue(self.D.adjacent('G', 'A'))
+        self.assertTrue(self.D.adjacent('E', 'D'))
+
+    def test_neighbors(self):
+        self.assertEqual(self.U.neighbors('I'), set(['A', 'C', 'B', 'E', 'D', 'G', 'F', 'H', 'J']))
+        self.assertEqual(self.D.neighbors('I'), set(['C', 'B', 'E', 'G', 'F', 'J']))
+
+    def test_degree(self):
+        self.assertEqual(self.U.degree('I', weighted=False), 11)
+        self.assertEqual(self.D.indegree('I', weighted=False), 3)
+        self.assertEqual(self.D.outdegree('I', weighted=False), 8)
+
 
 if __name__ == '__main__':
     unittest.main()
