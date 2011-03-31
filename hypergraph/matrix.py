6784f8136969bbc7180051b611fc1846b3b72be1
Aaron Mavrinac <mavrinac@gmail.com>
2011-03-31 10:03:38 -0400

    Add adjacency matrix function.

--- hypergraph/matrix.py
+++ hypergraph/matrix.py
@@ -14,8 +14,8 @@ def degree_matrix(H, weighted=True):
     """\
     Return the degree matrix of a hypergraph.
 
-    @param G: The input hypergraph.
-    @type G: L{Hypergraph}
+    @param H: The input hypergraph.
+    @type H: L{Hypergraph}
     @param weighted: Return the weighted degree matrix if true.
     @type weighted: C{bool}
     @return: The degree matrix.
@@ -23,3 +23,21 @@ def degree_matrix(H, weighted=True):
     """
     return numpy.diag([H.degree(v, weighted=weighted) \
         for v in sorted(list(H.vertices))])
+
+
+def adjacency_matrix(G):
+    """\
+    Return the adjacency matrix of a graph.
+
+    @param G: The input graph.
+    @type G: L{Graph}
+    @return: The adjacency matrix.
+    @rtype: C{numpy.ndarray}
+    """
+    assert G.uniform(2)
+    V = sorted(list(G.vertices))
+    adjacency = numpy.zeros((len(V), len(V)))
+    for u in range(len(V)):
+        for v in range(len(V)):
+            adjacency[u][v] = int(G.adjacent(V[u], V[v]))
+    return adjacency
